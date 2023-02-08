95a296f4b6020b1d81e4025e99c2d56cf7e3efb4
Zsiga Gergely <zsiga.g@freestart.hu>
2023-02-08 11:35:54 +0100

    Kész a feladat

--- src/app.js
+++ src/app.js
@@ -7,3 +7,26 @@
 * Github: https://github.com/zsi-ga/
 * Licenc: GNU GPL
 */
+const asideElem = document.querySelector("#aside");
+const bsideElem = document.querySelector("#bside");
+const csideElem = document.querySelector("#cside");
+
+const calcButton = document.querySelector("#calcButton");
+const areaElem = document.querySelector("#area");
+
+calcButton.addEventListener('click', () => {
+ 
+   let aside = Number(asideElem.value);
+   let bside = Number(bsideElem.value);
+   let cside = Number(csideElem.value);
+   
+
+   let area = calcArea(aside, bside, cside);
+   areaElem.value = area;
+});
+
+function calcArea(a, b, c) {
+    let terfogat = ((Math.PI/3)*a*b*c).toFixed(3)
+
+    return terfogat;
+}
